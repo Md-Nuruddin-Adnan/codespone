945e0c65aba3bcc5a8c3a0c9d85fff480f0c65e4
Md. Nuruddin Adnan <nuruddin.adnan1993@gmail.com>
2020-03-28 21:38:49 +0600

    index page responsive

--- js/custom.js
+++ js/custom.js
@@ -67,14 +67,21 @@ $(function(){
     slidesToScroll: 1,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 1200,
         settings: {
           slidesToShow: 4,
           slidesToScroll: 2,
         }
       },
       {
-        breakpoint: 600,
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 2,
+        }
+      },
+      {
+        breakpoint: 480,
         settings: {
           slidesToShow: 2,
           slidesToScroll: 2,
